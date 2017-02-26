250ccd746491c5ae45ad2985647f25a3f8014603
gopakumar <gopakumar.r@live.com>
2017-02-26 19:14:18 +0530

    updated the project json file

--- manageeasy.ui/Startup.cs
+++ manageeasy.ui/Startup.cs
@@ -4,14 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using manageeasy.ui.Data;
-using manageeasy.ui.Models;
-using manageeasy.ui.Services;
 
 namespace manageeasy.ui
 {
