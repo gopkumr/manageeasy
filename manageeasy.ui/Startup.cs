4dbf78f6ad90b31b0e7a41a71757f36a6c182b66
gopakumar <gopakumar.r@live.com>
2017-03-15 21:25:58 +0530

    Removing unwanted files

--- manageeasy.ui/Startup.cs
+++ manageeasy.ui/Startup.cs
@@ -9,9 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using manageeasy.ui.Data;
-using manageeasy.ui.Models;
-using manageeasy.ui.Services;
+
 
 namespace manageeasy.ui
 {
@@ -39,19 +37,8 @@ namespace manageeasy.ui
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
+            // Add mvc services.
             services.AddMvc();
-
-            // Add application services.
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
